fbd7f41e43c00e37f32c1f9b83d517a78f234f5d
Aldo2303 <aldod03@gmail.com>
2023-08-22 19:43:56 -0700

    update (/):

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 
     EMPLOYEE_NAME = emp_response.json()["name"]
 
-    print("Employee {} is done with tasks ({}/{})".
+    print("Employee {} is done with tasks ({}/{}):".
           format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
 
     for task in TASK_TITLE:
