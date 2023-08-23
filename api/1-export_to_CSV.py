ac49222b1b544ecd82d7331758eb477a71724b4c
Aldo2303 <aldod03@gmail.com>
2023-08-23 04:23:06 -0700

    task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+extend your Python script to export data in the CSV format
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    try:
+        emp_id = int(argv[1])
+    except Exception:
+        exit()
+
+    emp_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{emp_id}").json()
+    todos_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos").json()
+    
+    name = emp_response["username"]
+    file_name = f"{emp_id}.cvs"
+
+    with open(file_name, "w", encoding="utf-8") as file:
+        for task in todos_response:
+            task_completed = task.get("completed")
+            task_title = task.get("title")
+            file.write(
+                f'"{emp_id}", "{name}", "{task_completed}", "{task_title}"\n')
