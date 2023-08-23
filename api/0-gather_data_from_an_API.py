db8523f0a44eb62bd3f16e394d601a912b591789
Aldo2303 <aldod03@gmail.com>
2023-08-22 19:36:29 -0700

    update url, and problem with changed in git

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,29 +6,31 @@ information about his/her TODO list progress.
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-
-    base_url = (f"https://jsonplaceholder.typicode.com/")
-    try:
-        emp_id = int(argv[1])
-    except Exception:
-        exit()
-    emp_response = requests.get(f"{base_url}/users/{emp_id}")
-    todos_response = requests.get(f"{base_url}/todos?userId={emp_id}")
-
-    emp_data_json = emp_response.json()
-    EMPLOYEE_NAME = emp_data_json.get("name")
 
     NUMBER_OF_DONE_TASKS = 0
     TOTAL_NUMBER_OF_TASKS = 0
     TASK_TITLE = []
 
+    try:
+        emp_id = int(argv[1])
+    except Exception:
+        exit()
+
+    emp_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{emp_id}")
+    todos_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos")
+
     for task in todos_response.json():
-        TOTAL_NUMBER_OF_TASKS += 1
         if task.get("completed") is True:
             NUMBER_OF_DONE_TASKS += 1
+            TOTAL_NUMBER_OF_TASKS += 1
             TASK_TITLE.append(task.get("title"))
+        else:
+            TOTAL_NUMBER_OF_TASKS += 1
+
+    EMPLOYEE_NAME = emp_response.json()["name"]
 
     print("Employee {} is done with tasks ({}/{})".
           format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
